c5c8c0b228dad7dced85bbdebb6a592095c07b84
KarimGhaly <=>
2017-10-12 20:48:10 -0700

    Third

--- app/src/main/java/com/example/karim/finalproject/MainActivity.java
+++ app/src/main/java/com/example/karim/finalproject/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Karim", Toast.LENGTH_SHORT).show();
-
         Log.d(TAG, "onCreate: Edel Torres");
     }
 }
