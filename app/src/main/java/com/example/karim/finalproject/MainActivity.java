8cc3ecbbe92ae75c450a93db46ceb3a5cebd4af1
Edel <edelalexanderfb@gmail.com>
2017-10-12 23:41:52 -0400

    Added logd MainActivity.java

--- app/src/main/java/com/example/karim/finalproject/MainActivity.java
+++ app/src/main/java/com/example/karim/finalproject/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.karim.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    String TAG = "MainACtivityTag";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Karim", Toast.LENGTH_SHORT).show();
+
+        Log.d(TAG, "onCreate: Edel");
     }
 }
